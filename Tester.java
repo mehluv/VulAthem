ef90cfdd927cfe35f0c28884dec3988dd23126ca
mehluv <mehluv@gmail.com>
2016-10-23 19:34:45 +0530

    changed naive normalize function

--- Tester.java
+++ Tester.java
@@ -1,4 +1,5 @@
-
+//this is to
+//perform SVD and save to three files
 import Jama.*;
 import java.io.BufferedReader;
 import java.io.File;
@@ -7,6 +8,135 @@ import java.io.PrintWriter;
 
 public class Tester
 {
+
+    static void naiveNormalize(Matrix m)
+    {
+	double[][] msub = m.getArray();
+	double[] cAv = new double[msub[0].length];
+	double[] rAv = new double[msub.length];
+	for (int i = 0; i < msub[0].length; i++)
+	{
+	    int k = 0;
+	    for (int j = 0; j < msub.length; j++)
+	    {
+		if (msub[j][i] != 0)
+		{
+		    cAv[i] += msub[j][i];
+		    k++;
+		}
+	    }
+	    if (k == 0)
+	    {
+		cAv[i] = 3.0;
+		continue;
+	    }
+	    cAv[i] /= k;
+	}
+
+	for (int i = 0; i < msub.length; i++)
+	{
+	    int k = 0;
+	    rAv[i] = 0;
+	    for (int j = 0; j < msub[0].length; j++)
+	    {
+		if (msub[i][j] != 0)
+		{
+		    rAv[i] += msub[i][j];
+		    k++;
+		}
+	    }
+	    if (k != 0)
+	    {
+		rAv[i] /= k;
+	    }
+	}
+	for (int i = 0; i < msub[0].length; i++)
+	{
+	    for (int j = 0; j < msub.length; j++)
+	    {
+		if (msub[j][i] == 0)
+		{
+		    msub[j][i] = cAv[i];
+		}
+	    }
+	}
+	for (int i = 0; i < msub.length; i++)
+	{
+	    for (int j = 0; j < msub[0].length; j++)
+	    {
+//		System.out.println(rAv[i]);
+		msub[i][j] -= rAv[i];
+	    }
+	}
+    }
+
+    static void normalize(Matrix m)
+    {
+	double[][] msub = m.getArray();
+	for (int i = 0; i < msub[0].length; i++)
+	{
+	    double cAv = 0;
+	    int k = 0;
+	    for (int j = 0; j < msub.length; j++)
+	    {
+		if (msub[j][i] != 0)
+		{
+		    cAv += msub[j][i];
+		    k++;		//counting non zero columns
+		}
+	    }
+	    if (k == 0)
+	    {
+		for (int j = 0; j < msub.length; j++)
+		{
+		    msub[j][i] = 3.0;
+		}
+		continue;
+	    }
+	    cAv /= k;
+	    int sum = 0;
+	    for (int j = 0; j < msub.length; j++)
+	    {
+		if (msub[j][i] != 0)
+		{
+		    sum += (msub[j][i] - cAv) * (msub[j][i] - cAv);
+		}
+	    }
+	    cAv = sum / k;
+	    double sigma = Math.sqrt(cAv);
+	    for (int j = 0; j < msub.length; j++)
+	    {
+		if (msub[j][i] == 0)
+		{
+		    msub[j][i] = sigma;
+		}
+	    }
+	}
+
+	/*	for (int i = 0; i < msub.length; i++)
+	 {
+	 int rAv = 0;
+	 for (int j = 0; j < msub[0].length; j++)
+	 {
+	 rAv += msub[i][j];
+	 }
+	 rAv /= msub[0].length;
+	 int sum = 0;
+	 for (int j = 0; j < msub[0].length; j++)
+	 {
+
+	 sum += (msub[i][j] - rAv) * (msub[i][j] - rAv);
+
+	 }
+	 rAv = sum / msub[0].length;
+	 double sigma = Math.sqrt(rAv);
+	 for (int j = 0; j < msub[0].length; j++)
+	 {
+	 msub[i][j] -= sigma;
+	 }
+	 }*/
+	//Normalization Done
+    }
 
     static Matrix kReduction(Matrix m, int k, int k1)
     {
@@ -39,7 +169,7 @@ public class Tester
 	while (line != null)
 	{
 	    String[] sa = line.split(" ");
-	    msub[Integer.parseInt(sa[0])][Integer.parseInt(sa[1])] = Integer.parseInt(sa[2]);
+	    msub[Integer.parseInt(sa[0]) - 1][Integer.parseInt(sa[1]) - 1] = Integer.parseInt(sa[2]);
 	    ctr++;
 	    if ((ctr % 5000) == 0)
 	    {
@@ -52,68 +182,43 @@ public class Tester
 	    }
 	    line = br.readLine();
 	}
-	for (int i = 0; i < msub[0].length; i++)
-	{
-	    int cAv = 0;
-	    for (int j = 0; j < msub.length; j++)
-	    {
-		if (msub[j][i] != 0)
-		{
-		    cAv += msub[j][i];
-		}
-	    }
-	    cAv /= msub.length;
-	    for (int j = 0; j < msub.length; j++)
-	    {
-		if (msub[j][i] == 0)
-		{
-		    msub[j][i] = cAv;
-		}
-	    }
-	}
-	
-	for (int i = 0; i < msub.length; i++)
-	{
-	    int rAv = 0;
-	    for (int j = 0; j < msub[0].length; j++)
-	    {
-		rAv += msub[i][j];
-	    }
-	    rAv /= msub[0].length;
-	    for (int j = 0; j < msub[0].length; j++)
-	    {
-		msub[i][j] -= rAv;
-	    }
-	}
-	//Normalization Done
+
 	Matrix m = new Matrix(msub);
+	naiveNormalize(m);
+//	m.print(new PrintWriter(System.out), 4, 3);
+	//Normalization Done
 //	m.print(4, 3);
 	System.out.println("Performing SVD");
-	SingularValueDecomposition svd = m.svd();
+	SingularValueDecomposition svd = new SingularValueDecomposition(m);
 	System.out.println("Rank:- " + m.rank());
 	Matrix u = svd.getU(), v = svd.getV(), s = svd.getS();
-	System.out.println("Dimensions of U:- "+u.getRowDimension()+", "+u.getColumnDimension());
-	System.out.println("Dimensions of S:- "+s.getRowDimension()+", "+s.getColumnDimension());
-	System.out.println("Dimensions of V:- "+v.getRowDimension()+", "+v.getColumnDimension());
-/*	File fout = new File("SVD_U.dat");
+	System.out.println("Dimensions of U:- " + u.getRowDimension() + ", " + u.getColumnDimension());
+	System.out.println("Dimensions of S:- " + s.getRowDimension() + ", " + s.getColumnDimension());
+	System.out.println("Dimensions of V:- " + v.getRowDimension() + ", " + v.getColumnDimension());
+	File fout = new File("SVD_U.dat");
 	u.print(new PrintWriter(fout), 4, 3);
 	System.out.println("U:-");
-//	u.print(4, 3);
+	//	u.print(4, 3);
 	fout = new File("SVD_V.dat");
 	v.print(new PrintWriter(fout), 4, 3);
 	System.out.println("V:-");
-//	v.print(4, 3);
+	//	v.print(4, 3);
+	System.out.println("Creating S dat file");
 	fout = new File("SVD_S.dat");
-	double[][] sArr=new double[1][m.rank()];
-	for(int i=0;i<m.rank();i++)
-	    sArr[0][i]=s.get(i, i);
-	s=new Matrix(sArr);
+	System.out.println("Making the S array");
+	/*double[][] sArr = new double[1][m.rank()];
+	 for (int i = 0; i < m.rank(); i++)
+	 {
+	 sArr[0][i] = s.get(i, i);
+	 }
+	 s = new Matrix(sArr);
+	 s.print(new PrintWriter(fout), 4, 3);*/
 	s.print(new PrintWriter(fout), 4, 3);
 	System.out.println("S:-");
-//	s.print(4, 3);
-//	Matrix m1 = u.times(s).times(v);
-//	m1.print(4, 3);
-/*	int k = 2;
+	//	s.print(4, 3);
+	//	Matrix m1 = u.times(s).times(v);
+	//	m1.print(4, 3);
+	 /*	int k = 2;
 	 Matrix sk = kReduction(s, k, k);
 	 System.out.println("Reduced Sk");
 	 //	sk.print(4, 3);
