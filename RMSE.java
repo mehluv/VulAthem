3fad25f0e910119e0b3456d2031e3ea85ac45bb5
mehluv <mehluv@gmail.com>
2016-11-30 13:52:31 +0530

    and this too

--- RMSE.java
+++ RMSE.java
@@ -1,22 +1,34 @@
 
 import Jama.Matrix;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 class RMSE
 {
 
-    public static double rmse(Matrix trainingMat, Matrix testMat, TreeSet<Integer> ts)	//testMat is predicted,trainingMat is original
+    public static double rmse(Matrix trainingMat, Matrix testMat, TreeSet<Integer> rowList, ArrayList<Integer>[] definedSet)	//testMat is predicted,trainingMat is original
     {
 	int m = trainingMat.getRowDimension();
 	int n = trainingMat.getColumnDimension();
 	double s = 0.0;
-	for (int i in ts)
+	int ctr=0;
+	for (int i : rowList)
 	{
-	    for (int j = 0; j < n; j++)
+	    for (int j: definedSet[ctr])
 	    {
-		s += Math.pow((trainingMat.get(i, j) - testMat.get(i, j)), 2);
+		double a=(trainingMat.get(i, j));
+		double b=testMat.get(i, j);
+		s += Math.pow((a - b), 2);
 	    }
+	    ctr++;
 	}
-	double error = Math.sqrt(s / (m * n));
+	
+	int noOfRatings=0;
+	for (ArrayList<Integer> ar: definedSet)
+	{
+	    noOfRatings+=ar.size();
+	}
+	double error = Math.sqrt(s/noOfRatings);///m;// / (m * n));
 
 	return error;
     }
