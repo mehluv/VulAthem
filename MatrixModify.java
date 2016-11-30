2d24e27e8ca68c986fcb3e5945e7d0e9e2a9eaf1
mehluv <mehluv@gmail.com>
2016-11-30 13:51:58 +0530

    CHanged this as well

--- MatrixModify.java
+++ MatrixModify.java
@@ -1,6 +1,10 @@
 
 import Jama.Matrix;
 import Jama.SingularValueDecomposition;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.TreeSet;
 
@@ -9,78 +13,173 @@ import java.util.TreeSet;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author luv
  */
 public class MatrixModify
 {
-	static TreeSet<Integer> st=null;
-    public static Matrix makeTestMatrix(Matrix mat)
+
+    static TreeSet<Integer> testRows = null;
+    static ArrayList[] definedList;
+
+    public static Matrix makeMatrices(File fi, Matrix mat) throws Exception
     {
-	if(mat.getRowDimension()<mat.getColumnDimension())
+	boolean flag = false;
+	//File tha shit
+	int index = 1;
+	double[][] msub = mat.getArray();
+	for (File f : fi.listFiles())
 	{
-	    return makeTestMatrix(mat.transpose());
-	}
-	int n=mat.getRowDimension()/5;
-	st=new TreeSet<>();
-	Random r=new Random();
-	while(st.size()<n)
-	{
-	    st.add(r.nextInt(mat.getRowDimension()));
-	}
-	double[][] msub=mat.getArray();
-	for(int i:st)
-	{
-	    msub[i]=new double[mat.getColumnDimension()];
-	}
-	double[] cAv = new double[msub[0].length];
-	for (int i = 0; i < msub[0].length; i++)
-	{
-	    int k = 0;
-	    for (int j = 0; j < msub.length; j++)
+	    BufferedReader br = new BufferedReader(new FileReader(f));
+	    String line = br.readLine();
+	    int pre = -1;
+	    int dimRow = 0;
+	    ArrayList<Integer> userList = new ArrayList<>();
+	    while (line != null)
 	    {
-		if (msub[j][i] != 0)
+		String s[] = line.split(" ");
+		int comp1 = Integer.parseInt(s[1]);
+		if (comp1 != pre)
 		{
-		    cAv[i] += msub[j][i];
-		    k++;
+		    pre = comp1;
+		    dimRow++;
+		}
+		line = br.readLine();
+	    }
+	    br = new BufferedReader(new FileReader(f));
+	    line = br.readLine();
+	    pre = -1;
+	    int rowPtr = -1;
+	    while (line != null)
+	    {
+		String[] sa = line.split(" ");
+
+		if (pre != Integer.parseInt(sa[1]))
+		{
+		    rowPtr++;
+		    pre = Integer.parseInt(sa[1]);
+		    userList.add(pre);
+		}
+		msub[rowPtr][Integer.parseInt(sa[2]) - 1] = Integer.parseInt(sa[3]);
+		line = br.readLine();
+
+	    }
+	    index++;
+	}
+	int n = mat.getRowDimension() / 5;
+	testRows = new TreeSet<>();
+	Random r = new Random();
+	while (testRows.size() < n)
+	{
+	    testRows.add(r.nextInt(mat.getRowDimension()));
+	}
+	definedList = new ArrayList[n];
+	int ctr = 0;
+	for (Integer i : testRows)
+	{
+	    definedList[ctr] = new ArrayList<Integer>();
+	    for (int j = 0; j < 3952; j++)
+	    {
+		if (mat.get(i, j) != 0)
+		{
+		    definedList[ctr].add(j);
 		}
 	    }
-	    if (k == 0)
-	    {
-		cAv[i] = 3.0;
-		continue;
-	    }
-	    cAv[i] /= k;
+	    ctr++;
 	}
-	Tester.naiveNormalize(mat);
-	System.out.println("Performing SVD");
-	SingularValueDecomposition svd = new SingularValueDecomposition(mat);
-	//System.out.println("Rank:- " + mat.rank());
-	int rank = mat.rank();
-	Matrix u = svd.getU(), v = svd.getV(), s = svd.getS();
-	System.out.println("Dimensions of U:- " + u.getRowDimension() + ", " + u.getColumnDimension());
-	System.out.println("Dimensions of S:- " + s.getRowDimension() + ", " + s.getColumnDimension());
-	System.out.println("Dimensions of V:- " + v.getRowDimension() + ", " + v.getColumnDimension());
-	int k = mat.rank() / 7;
-	Matrix sk = Tester.kReduction(s, k, k);
-	System.out.println("Reduced Sk");
-	//	sk.print(4, 3);
-	Matrix uk = Tester.kReduction(u, u.getRowDimension(), k);
-	System.out.println("Reduced Uk");
-	//	uk.print(4, 3);
-	Matrix vk = Tester.kReduction(v, v.getRowDimension(), k);
-	System.out.println("Reduced Vk");
-	System.out.println("Finding resultant prediction matrix:-");
-	Matrix res = uk.times(sk.times(vk.transpose()));
-	for (int j = 0; j < res.getColumnDimension(); j++)
+	//now perform prediction
+	index = 1;
+	for (File f : fi.listFiles())
 	{
-	    for (int i = 0; i < res.getRowDimension(); i++)
+	    System.out.println(f);
+	    SVDBatchMil.work(f, index);
+	    index++;
+	}
+	msub = new double[6040][3952];
+	Matrix res = new Matrix(msub);
+	for (int i = 1; i < index; i++)
+	{
+	    File f = new File("temp/predictedMatrixFor1MDiv" + i + ".dat");
+	    BufferedReader br = new BufferedReader(new FileReader(f));
+	    String[] sa = br.readLine().split(" ");
+	    int userList[] = new int[sa.length];
+	    for (int j = 0; j < sa.length; j++)
 	    {
-		res.set(i, j, res.get(i, j) + cAv[j]);
+		userList[j] = Integer.parseInt(sa[j]);
+	    }
+	    String line = br.readLine();
+	    line = br.readLine();
+	    int rowPtr;
+	    System.out.println(fi);
+	    for (int k = 0; k < sa.length; k++)
+	    {
+		String[] rPred = line.split(" ");
+		rowPtr = userList[k];
+		for (int j = 0; j < 1681; j++)
+		{
+		    msub[rowPtr][j] = Double.parseDouble(rPred[j + 1]);
+		}
+		line = br.readLine();
 	    }
 	}
+	/*	if (mat.getRowDimension() < mat.getColumnDimension())
+	 {
+	 mat = mat.transpose();
+	 flag = true;
+	 }
+	
+	 double[][] msub = mat.getArray();
+	 for (int i : testRows)
+	 {
+	 msub[i] = new double[mat.getColumnDimension()];
+	 }
+	 double[] cAv = new double[msub[0].length];
+	 for (int i = 0; i < msub[0].length; i++)
+	 {
+	 int k = 0;
+	 for (int j = 0; j < msub.length; j++)
+	 {
+	 if (msub[j][i] != 0)
+	 {
+	 cAv[i] += msub[j][i];
+	 k++;
+	 }
+	 }
+	 if (k == 0)
+	 {
+	 cAv[i] = 3.0;
+	 continue;
+	 }
+	 cAv[i] /= k;
+	 }
+	 Tester.naiveNormalize(mat);
+	 System.out.println("Performing SVD");
+	 SingularValueDecomposition svd = new SingularValueDecomposition(mat);
+	 //System.out.println("Rank:- " + mat.rank());
+	 int rank = mat.rank();
+	 Matrix u = svd.getU(), v = svd.getV(), s = svd.getS();
+	 System.out.println("Dimensions of U:- " + u.getRowDimension() + ", " + u.getColumnDimension());
+	 System.out.println("Dimensions of S:- " + s.getRowDimension() + ", " + s.getColumnDimension());
+	 System.out.println("Dimensions of V:- " + v.getRowDimension() + ", " + v.getColumnDimension());
+	 int k = mat.rank() / 7;
+	 Matrix sk = Tester.kReduction(s, k, k);
+	 System.out.println("Reduced Sk");
+	 //	sk.print(4, 3);
+	 Matrix uk = Tester.kReduction(u, u.getRowDimension(), k);
+	 System.out.println("Reduced Uk");
+	 //	uk.print(4, 3);
+	 Matrix vk = Tester.kReduction(v, v.getRowDimension(), k);
+	 System.out.println("Reduced Vk");
+	 System.out.println("Finding resultant prediction matrix:-");
+	 Matrix res = uk.times(sk.times(vk.transpose()));
+	 
+	 if (flag)
+	 {
+	 return res.transpose();
+	 }
+	 */
+	
 	return res;
     }
 }
